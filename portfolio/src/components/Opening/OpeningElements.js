421a460a03a0d60d61353b11778b45ced445fc8e
Yuya Hashirizaki <hashirizaki61@gmail.com>
2021-01-06 23:36:54 -0800

    changed with from old title code to incorporated react framer

--- portfolio/src/components/Opening/OpeningElements.js
+++ portfolio/src/components/Opening/OpeningElements.js
@@ -1,58 +1,83 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-export const OpeningContainer = styled.div`
-  background: rgb(255, 255, 255);
-  background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 1) 0%,
-    rgba(218, 218, 218, 1) 100%
-  );
+export const Section = styled.section`
+  height: 800px;
   display: flex;
   justify-content: center;
-  justify-content: flex-end;
   align-items: center;
-  padding: 0 10px;
-  height: 800px;
-  position: relative;
-  z-index: 1;
+  background: #fff;
 `;
 
-export const OpeningContent = styled.div`
-  z-index: 2;
-  max-width: 1200px;
-  position: absolute;
-  padding: 8px 24px;
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+  padding: 3rem calc((100vw - 1300px) / 2);
+
+  @media screen and (max-width: 768px) {
+    grid-grid-template-columns: 1fr;
+  }
+`;
+
+export const ColumnLeft = styled.div`
   display: flex;
+  color: #444;
   flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 5rem 2rem;
+
+  h1 {
+    margin-bottom: 0.5rem;
+    font-size: 2rem;
+  }
+
+  p {
+    margin: 2rem 0;
+    font-size: 4rem;
+    line-height: 1.1;
+  }
+`;
+
+export const Image = styled(motion.img)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  max-width: 100px;
+  max-height: 100px;
+  filter: drop-shadow(2px 3px 5px rgba(0, 0, 0, 0.2));
+`;
+
+export const ColumnRight = styled.div`
+  display: flex;
+  justify-content: center;
   align-items: center;
-`;
+  padding: 2rem;
+  position: relative;
 
-export const OpeningH1 = styled.h1`
-  color: #010606;
-  font-size: 48px;
-  text-align: center;
-
-  @media screen and (max-width: 768px) {
-    font-size: 40px;
+  ${Image}:nth-child(1) {
+    top: 70px;
+    left: 10px;
   }
 
-  @media screen and (max-width: 480px) {
-    font-size: 32px;
-  }
-`;
-
-export const OpeningP = styled.p`
-  margin-top: 24px;
-  color: #010606;
-  font-size: 24px;
-  text-align: center;
-  max-width: 600px;
-
-  @media screen and (max-width: 768px) {
-    font-size: 24px;
+  ${Image}:nth-child(2) {
+    top: 170px;
+    right: 10px;
   }
 
-  @media screen and (max-width: 480px) {
-    font-size: 18px;
+  ${Image}:nth-child(3) {
+    top: 320px;
+    left: 90px;
+  }
+
+  ${Image}:nth-child(4) {
+    bottom: 100px;
+    right: 500px;
+  }
+
+  ${Image}:nth-child(5) {
+    bottom: 70px;
+    right: 100px;
   }
 `;
--- portfolio/src/components/Opening/index.js
+++ portfolio/src/components/Opening/index.js
@@ -1,25 +1,93 @@
 import React, { useState } from "react";
 import {
-  OpeningContainer,
-  OpeningContent,
-  OpeningH1,
-  OpeningP,
+  Section,
+  Container,
+  ColumnLeft,
+  ColumnRight,
+  Image,
 } from "./OpeningElements";
+import { motion } from "framer-motion";
+import PlanetOne from "../../images/ZigZag_1.svg";
+import PlanetTwo from "../../images/ZigZag_2.svg";
+import PlanetThree from "../../images/ZigZag_3.svg";
+import PlanetFour from "../../images/ZigZag_4.svg";
+import PlanetFive from "../../images/ZigZag_5.svg";
 
 const OpeningSection = () => {
+  const fadeLeft = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
-    <OpeningContainer id="home">
-      <OpeningContent>
-        <OpeningH1>I am Yuya Hashirizaki</OpeningH1>
-        <OpeningH1>
-          I design & build digital products based in Vancouver
-        </OpeningH1>
-        <OpeningP>
-          I’m a passionate multi-talented human who comes from Japan with over
-          6+ years of experiences in wider field of design disciplines
-        </OpeningP>
-      </OpeningContent>
-    </OpeningContainer>
+    <Section>
+      <Container>
+        <ColumnLeft>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            Welcome to Space
+          </motion.h1>
+          <motion.p
+            variants={fadeLeft}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 1 }}
+          >
+            Journey to the unknown
+          </motion.p>
+        </ColumnLeft>
+        <ColumnRight>
+          <Image
+            src={PlanetOne}
+            alt="planet"
+            whileTap={{ scale: 0.9 }}
+            drag={true}
+            dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50 }}
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          />
+          <Image
+            src={PlanetTwo}
+            alt="planet"
+            whileTap={{ scale: 0.9 }}
+            drag={true}
+            dragConstraints={{ left: 50, right: 0, top: 0, bottom: 50 }}
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+          />
+          <Image
+            src={PlanetThree}
+            alt="planet"
+            whileTap={{ scale: 0.8 }}
+            drag={true}
+            dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50 }}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+          />
+          <Image
+            src={PlanetFour}
+            alt="planet"
+            whileTap={{ scale: 0.9 }}
+            drag={true}
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          />
+          <Image
+            src={PlanetFive}
+            alt="planet"
+            whileTap={{ scale: 0.9 }}
+            drag={true}
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          />
+        </ColumnRight>
+      </Container>
+    </Section>
   );
 };
 

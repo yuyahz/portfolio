dece8968f8c4fe3636f40ce3f9e1b299a1cf2292
Yuya Hashirizaki <hashirizaki61@gmail.com>
2021-01-16 17:35:35 -0800

    ongoing tweak position of zig zag

--- portfolio/src/components/Opening/OpeningElements.js
+++ portfolio/src/components/Opening/OpeningElements.js
@@ -12,16 +12,26 @@ export const Section = styled.section`
     rgba(255, 255, 255, 1) 0%,
     rgba(218, 218, 218, 1) 100%
   );
+
+  /* for Surface duo to eliminate right space */
+  @media screen and (max-width: 540px) {
+    overflow: hidden;
+  }
+
+  @media screen and (max-width: 520px) {
+    overflow: hidden;
+  }
 `;
 
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   height: 100vh;
-  padding: 3rem calc((100vw - 1300px) / 2);
+  /* padding: 3rem calc((100vw - 1300px) / 2); */
+  padding: 3rem;
 
-  @media screen and (max-width: 768px) {
-    grid-grid-template-columns: 1fr;
+  @media screen and (max-width: 520px) {
+    grid-template-columns: 1fr;
   }
 `;
 
@@ -32,12 +42,12 @@ export const ColumnLeft = styled.div`
   justify-content: center;
   align-items: flex-start;
   padding: 1rem 3rem;
+  /* background: skyblue; */
 
   h1 {
     font-weight: 900;
     margin-bottom: 0.5rem;
     font-size: 9rem;
-    z-index: 100;
     -ms-user-select: none; /* IE 10+ */
     -moz-user-select: -moz-none;
     -khtml-user-select: none;
@@ -50,7 +60,6 @@ export const ColumnLeft = styled.div`
     margin: 2rem 0 3rem 0;
     font-size: 7rem;
     line-height: 0.9;
-    z-index: 100;
     -ms-user-select: none; /* IE 10+ */
     -moz-user-select: -moz-none;
     -khtml-user-select: none;
@@ -63,7 +72,6 @@ export const ColumnLeft = styled.div`
     margin-top: 1rem;
     font-size: 1rem;
     letter-spacing: 0.7rem;
-    z-index: 100;
     -ms-user-select: none; /* IE 10+ */
     -moz-user-select: -moz-none;
     -khtml-user-select: none;
@@ -71,30 +79,19 @@ export const ColumnLeft = styled.div`
     user-select: none;
   }
 
-  /* for vertical Ipad pro */
-  @media screen and (max-width: 1024px) {
-    padding: 1rem 6rem;
-  }
-
-  @media screen and (max-width: 480px) {
+  @media screen and (max-width: 520px) {
     h1 {
-      font-weight: 900;
-      margin-bottom: 0.5rem;
-      font-size: 7rem;
+      margin-bottom: 0rem;
+      font-size: 8rem;
     }
-
     h2 {
-      font-weight: 900;
-      margin: 2rem 0;
-      font-size: 7rem;
+      margin: 0 0 1rem 0;
+      font-size: 5rem;
       line-height: 0.9;
     }
-
     p {
-      font-weight: 100;
-      margin: 2rem 0;
-      font-size: 1rem;
-      letter-spacing: 1.3rem;
+      font-size: 0.7rem;
+      letter-spacing: 0.5rem;
     }
   }
 `;
@@ -104,13 +101,6 @@ export const Image = styled(motion.img)`
   width: auto;
   height: auto;
   filter: drop-shadow(2px 3px 5px rgba(0, 0, 0, 0.2));
-
-  @media screen and (max-width: 768px) {
-  }
-
-  @media screen and (max-width: 480px) {
-    max-width: 80%;
-  }
 `;
 
 export const ColumnRight = styled.div`
@@ -119,6 +109,7 @@ export const ColumnRight = styled.div`
   align-items: center;
   padding: 2rem;
   position: relative;
+  /* background: pink; */
 
   ${Image}:nth-child(1) {
     top: 150px;
@@ -145,58 +136,71 @@ export const ColumnRight = styled.div`
     right: 120px;
   }
 
-  /* for horizontal Ipad pro */
-  /* @media screen and (max-width: 1366px) {
+  /* for Tablet */
+  @media screen and (max-width: 960px) {
     ${Image}:nth-child(1) {
-    top: 280px;
-    right: 150px;
-  }
-
-  ${Image}:nth-child(2) {
-    bottom: 320px;
-    right: 280px;
-  }
-
-  ${Image}:nth-child(3) {
-    top: 380px;
-    right: 390px;
-  }
-
-  ${Image}:nth-child(4) {
-    bottom: 430px;
-    right: 220px;
-  }
-
-  ${Image}:nth-child(5) {
-    bottom: 300px;
-    right: 120px;
-  } */
-
-  /* for Vertical Ipad pro */
-  /* @media screen and (max-width: 1024px) {
-    ${Image}:nth-child(1) {
-      top: 450px;
-      right: 150px;
+      top: 150px;
+      right: 120px;
     }
 
     ${Image}:nth-child(2) {
-      bottom: 550px;
-      right: 280px;
+      bottom: 180px;
+      right: 250px;
     }
 
     ${Image}:nth-child(3) {
-      top: 520px;
-      right: 380px;
+      top: 230px;
+      right: 350px;
     }
 
     ${Image}:nth-child(4) {
-      bottom: 630px;
-      right: 220px;
+      bottom: 300px;
+      right: 190px;
     }
 
     ${Image}:nth-child(5) {
-      bottom: 500px;
-      right: 150px;
+      bottom: 200px;
+      right: 90px;
     }
-  } */
+  }
+
+  /* for Mobile */
+  @media screen and (max-width: 520px) {
+    padding: 0rem;
+
+    ${Image}:nth-child(1) {
+      max-width: 30%;
+      height: auto;
+      top: -300px;
+      right: 10px;
+    }
+
+    ${Image}:nth-child(2) {
+      max-width: 9%;
+      height: auto;
+      bottom: 290px;
+      right: 80px;
+    }
+
+    ${Image}:nth-child(3) {
+      max-width: 9%;
+      height: auto;
+      top: -180px;
+      right: 10px;
+    }
+
+    ${Image}:nth-child(4) {
+      max-width: 9%;
+      height: auto;
+      bottom: 320px;
+      right: 50px;
+    }
+
+    ${Image}:nth-child(5) {
+      max-width: 9%;
+      height: auto;
+      bottom: 230px;
+      right: 45px;
+    }
+  }
 `;

71bfafb17af93914a9f1ac05a6c53baa61157c7a
Yuya Hashirizaki <hashirizaki61@gmail.com>
2021-02-18 23:54:24 -0800

    ongoing prallax and delete title sec css

--- portfolio/src/components/TitleSection/TitleElements.js
+++ portfolio/src/components/TitleSection/TitleElements.js
@@ -31,16 +31,12 @@ export const TitleContent = styled.div`
   z-index: 2;
   width: 100%;
   max-width: 1440px;
-  position: absolute;
-  padding: 8px 24px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
+  display: block;
 `;
 
 export const ImageContent = styled.div`
   z-index: 2;
-  padding: 8px 24px;
+  padding: 0 3%;
   display: flex;
   justify-content: flex-start;
   flex-direction: column;
@@ -50,8 +46,7 @@ export const ImageContent = styled.div`
 `;
 
 export const Image = styled.img`
-  width: 100%;
-  max-width: 100%;
+  max-width: 70%;
   height: auto;
 `;
 
@@ -76,10 +71,11 @@ export const TitleH1 = styled.h1`
   );
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  margin: 0 auto;
   font-weight: 900;
   font-size: 60px;
   text-align: left;
-  max-width: 1000px;
+  max-width: 870px;
 
   .br {
     white-space: pre-wrap;
@@ -87,6 +83,7 @@ export const TitleH1 = styled.h1`
 
   @media screen and (max-width: 970px) {
     font-size: 32px;
+    max-width: 480px;
   }
 
   @media screen and (max-width: 520px) {
@@ -109,16 +106,19 @@ export const PContent = styled.div`
 export const TitleP = styled.p`
   color: #010606;
   letter-spacing: 0.05em;
+  margin: 0 auto;
   font-size: 24px;
   font-weight: 300;
   text-align: left;
-  max-width: 550px;
+  max-width: 525px;
 
   @media screen and (max-width: 970px) {
     font-size: 16px;
+    max-width: 350px;
   }
 
   @media screen and (max-width: 520px) {
     font-size: 14px;
+    max-width: 200px;
   }
 `;

da5374bf7a5ccf2c1c0e2119bf05bec10e29c799
Yuya Hashirizaki <hashirizaki61@gmai.com>
2024-08-04 23:04:40 -0700

    fix rendering issue ParallaxSmv2

--- src/components/ParallaxComponentSmv2.js
+++ src/components/ParallaxComponentSmv2.js
@@ -1,21 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import ParallaxBg from "../images/ParallaxSmv2.png"; // Ensure correct path
 
 const ParallaxContainer = styled.div`
   width: 100%;
-  background: var(--secondary-bg);
-
-  @media only screen and (min-width: 959.99px) {
-    display: none;
-  }
 `;
 
 const ParallaxWrapper = styled.div`
   width: 100%;
-  background: var(--seconday-bg);
+  z-index: 1;
 `;
 
-const Section = styled.section`
+const ParallaxSection = styled.section`
   position: relative;
   width: 100%;
   height: 100vh;
@@ -26,14 +22,16 @@ const Section = styled.section`
     left: 0;
     width: 100%;
     height: 100%;
-    clip: rect(auto, auto, auto, auto);
+    background: var(--secondary-bg);
+    clip: rect(auto, auto, auto, auto); /* This clips the background */
+    z-index: 1;
 
     p {
       font-size: 20vw;
       color: var(--fourth-txt-color);
       font-family: "GT";
       font-weight: 300;
-      z-index: 1;
+      margin: 0 auto;
     }
 
     @media only screen and (max-width: 540px) {
@@ -48,50 +46,51 @@ const Section = styled.section`
     }
   }
 
-  .underLayer {
+  .ParallaxBg {
     position: fixed;
     top: 0;
     left: 0;
-    /* z-index: -1; */
+    z-index: -1;
     display: block;
     width: 100%;
-    height: 100vh;
+    height: 50vh;
     background-size: cover;
     background-position: center;
-    background: var(--secondary-bg);
 
-    .moon {
-      padding-bottom: 4.167vw;
-      /* animation-name: sway;
-      animation-duration: 4s;
-      animation-direction: alternate;
-      animation-iteration-count: infinite;
-      animation-timing-function: linear;
-      @keyframes sway {
-        from {
-          padding-bottom: 4.167vw;
-        }
-        to {
-          padding-bottom: 7.161vw;
-        }
-      } */
+    .txtCenter {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      text-align: center;
+    }
+
+    .vertical {
+      display: block;
     }
   }
 
-  .underLayerTxt {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    text-align: center;
-  }
-
-  .LayerFrame {
+  .LayerFrameTop {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    width: 100%;
+    height: 50vh;
+    margin-top: 50vh;
+    border-bottom: solid 6px #fe0000;
   }
+
+  /* .LayerFrameBottm {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100%;
+    height: 0;
+    margin-top: 50vh;
+    background: rgba(255, 255, 255, 0.8);
+  } */
 
   span {
     padding: 5vw 50vw;
@@ -106,42 +105,212 @@ const Section = styled.section`
       rgba(254, 0, 0, 0) 30%
     );
   }
+
+  .moon {
+    padding-bottom: 4.167vw;
+    /* animation-name: sway;
+    animation-duration: 4s;
+    animation-direction: alternate;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    @keyframes sway {
+      from {
+        padding-bottom: 4.167vw;
+      }
+      to {
+        padding-bottom: 7.161vw;
+      }
+    } */
+  }
 `;
 
 const ParallaxComponentSmv2 = () => {
   return (
     <ParallaxContainer>
       <ParallaxWrapper>
-        <Section>
+        <ParallaxSection>
           <div className="sectionInner">
-            <div className="underLayer">
-              <div className="underLayerTxt">
+            <div
+              className="ParallaxBg"
+              style={{ backgroundImage: `url(${ParallaxBg})` }}
+            >
+              <div className="txtCenter">
                 <p>Let's Make</p>
               </div>
             </div>
-            <div className="LayerFrame">
-              <p></p>
+            <div className="LayerFrameTop">
+              <p>Something</p>
             </div>
           </div>
-        </Section>
-        <Section>
+        </ParallaxSection>
+        <ParallaxSection>
           <div className="sectionInner">
-            <div className="underLayer">
-              <div className="underLayerTxt">
-                <div>
+            <div
+              className="ParallaxBg"
+              style={{ backgroundImage: `url(${ParallaxBg})` }}
+            >
+              <div className="txtCenter">
+                <div className="vertical">
                   <p className="moon">Amazing</p>
                   <span></span>
                 </div>
               </div>
             </div>
-            <div className="LayerFrame">
-              <p>Something</p>
-            </div>
+            {/* <div className="LayerFrameBottom"></div> */}
           </div>
-        </Section>
+        </ParallaxSection>
       </ParallaxWrapper>
     </ParallaxContainer>
   );
 };
 
 export default ParallaxComponentSmv2;
+
+//v1//
+// const ParallaxContainer = styled.div`
+//   width: 100%;
+//   background: var(--secondary-bg);
+
+//   @media only screen and (min-width: 959.99px) {
+//     display: none;
+//   }
+// `;
+
+// const ParallaxWrapper = styled.div`
+//   width: 100%;
+//   background: var(--seconday-bg);
+// `;
+
+// const Section = styled.section`
+//   position: relative;
+//   width: 100%;
+//   height: 100vh;
+
+//   .sectionInner {
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     clip: rect(auto, auto, auto, auto);
+
+//     p {
+//       font-size: 20vw;
+//       color: var(--fourth-txt-color);
+//       font-family: "GT";
+//       font-weight: 300;
+//       z-index: 1;
+//     }
+
+//     @media only screen and (max-width: 540px) {
+//       p {
+//         font-size: 19vw;
+//       }
+//     }
+//     @media only screen and (max-width: 369px) {
+//       p {
+//         font-size: 17.5vw;
+//       }
+//     }
+//   }
+
+//   .underLayer {
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     /* z-index: -1; */
+//     display: block;
+//     width: 100%;
+//     height: 100vh;
+//     background-size: cover;
+//     background-position: center;
+//     background: var(--secondary-bg);
+
+//     .moon {
+//       padding-bottom: 4.167vw;
+//       animation-name: sway;
+//       animation-duration: 4s;
+//       animation-direction: alternate;
+//       animation-iteration-count: infinite;
+//       animation-timing-function: linear;
+//       @keyframes sway {
+//         from {
+//           padding-bottom: 4.167vw;
+//         }
+//         to {
+//           padding-bottom: 7.161vw;
+//         }
+//       }
+//     }
+//   }
+
+//   .underLayerTxt {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     height: 100vh;
+//     text-align: center;
+//   }
+
+//   .LayerFrame {
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+//     width: 100%;
+//     margin-top: 100vh;
+//     margin-top: auto;
+//   }
+
+//   span {
+//     padding: 5vw 50vw;
+//     margin-bottom: 20vw;
+//     border-radius: 50% 50% 0 0;
+//     background: rgb(250, 248, 246);
+//     background: linear-gradient(
+//       180deg,
+//       rgba(250, 248, 246, 1) 0%,
+//       rgba(251, 167, 166, 1) 10%,
+//       rgba(252, 108, 107, 1) 20%,
+//       rgba(254, 0, 0, 0) 30%
+//     );
+//   }
+// `;
+
+// const ParallaxComponentSmv2 = () => {
+//   return (
+//     <ParallaxContainer>
+//       <ParallaxWrapper>
+//         <Section>
+//           <div className="sectionInner">
+//             <div className="underLayer">
+//               <div className="underLayerTxt">
+//                 <p>Let's Make</p>
+//               </div>
+//             </div>
+//             <div className="LayerFrame">
+//               <p></p>
+//             </div>
+//           </div>
+//         </Section>
+//         <Section>
+//           <div className="sectionInner">
+//             <div className="underLayer">
+//               <div className="underLayerTxt">
+//                 <div>
+//                   <p className="moon">Amazing</p>
+//                   <span></span>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="LayerFrame">
+//               <p>Something</p>
+//             </div>
+//           </div>
+//         </Section>
+//       </ParallaxWrapper>
+//     </ParallaxContainer>
+//   );
+// };
+
+// export default ParallaxComponentSmv2;

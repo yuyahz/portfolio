3c027f212d639f7bde847b820ecdb5d9ac21bbb6
Yuya Hashirizaki <hashirizaki61@gmai.com>
2024-08-08 17:47:28 -0700

    revert test code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -62,7 +62,7 @@ const Home = () => {
             animate="visible"
             transition={{ duration: 2 }}
           >
-            HH
+            H
           </motion.h1>
           <ImageWin
             src={win}
